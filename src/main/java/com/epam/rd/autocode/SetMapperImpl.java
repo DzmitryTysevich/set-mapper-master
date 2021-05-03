13ccaf95b1b8cfc9a9e427793b2c405aefb8dbce
DzmitryTysevich <dztysevich@gmail.com>
2021-05-03 20:12:46 +0300

    add manager method

--- src/main/java/com/epam/rd/autocode/SetMapperImpl.java
+++ src/main/java/com/epam/rd/autocode/SetMapperImpl.java
@@ -10,6 +10,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SetMapperImpl implements SetMapper<Set<Employee>> {
+
+    private static final String ID = "id";
+    private static final String FIRST_NAME = "firstname";
+    private static final String LAST_NAME = "lastname";
+    private static final String MIDDLE_NAME = "middleName";
+    private static final String POSITION = "position";
+    private static final String HIREDATE = "hiredate";
+    private static final String SALARY = "salary";
+    private static final String JDBC_DRIVER = "org.hsqldb.jdbc.JDBCDriver";
+    private static final String URL = "jdbc:hsqldb:mem:myDb";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "sa";
+    private static final String SQL_SELECT = "select * from EMPLOYEE E left join EMPLOYEE N on E.MANAGER = N.ID";
+    private static final String MANAGER = "manager";
+
     @Override
     public Set<Employee> mapSet(ResultSet resultSet) {
         Set<Employee> employees = new HashSet<>();
@@ -25,14 +40,14 @@ public class SetMapperImpl implements SetMapper<Set<Employee>> {
 
     private Employee getEmployee(ResultSet resultSet) throws SQLException {
         return new Employee(
-                new BigInteger(String.valueOf(resultSet.getInt("id"))),
+                new BigInteger(String.valueOf(resultSet.getInt(ID))),
                 new FullName(
-                        resultSet.getString("firstname"),
-                        resultSet.getString("lastname"),
-                        resultSet.getString("middleName")),
-                Position.valueOf(resultSet.getString("position")),
-                resultSet.getDate("hiredate").toLocalDate(),
-                resultSet.getBigDecimal("salary"),
+                        resultSet.getString(FIRST_NAME),
+                        resultSet.getString(LAST_NAME),
+                        resultSet.getString(MIDDLE_NAME)),
+                Position.valueOf(resultSet.getString(POSITION)),
+                resultSet.getDate(HIREDATE).toLocalDate(),
+                resultSet.getBigDecimal(SALARY),
                 getManager(resultSet)
         );
     }
@@ -40,28 +55,28 @@ public class SetMapperImpl implements SetMapper<Set<Employee>> {
     private Employee getManager(ResultSet resultSet) throws SQLException {
         Employee manager = null;
         try {
-            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            Class.forName(JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         try {
             Connection connection = DriverManager.getConnection(
-                    "jdbc:hsqldb:mem:myDb",
-                    "sa", "sa"
+                    URL,
+                    USER, PASSWORD
             );
             Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("select * from EMPLOYEE E left join EMPLOYEE N on E.MANAGER = N.ID");
+            ResultSet rs = statement.executeQuery(SQL_SELECT);
             while (rs.next()) {
-                if (resultSet.getInt("manager") == (rs.getInt("id"))) {
+                if (resultSet.getInt(MANAGER) == (rs.getInt(ID))) {
                     manager = new Employee(
-                            new BigInteger(String.valueOf(rs.getInt("id"))),
+                            new BigInteger(String.valueOf(rs.getInt(ID))),
                             new FullName(
-                                    rs.getString("firstname"),
-                                    rs.getString("lastname"),
-                                    rs.getString("middleName")),
-                            Position.valueOf(rs.getString("position")),
-                            rs.getDate("hiredate").toLocalDate(),
-                            rs.getBigDecimal("salary"),
+                                    rs.getString(FIRST_NAME),
+                                    rs.getString(LAST_NAME),
+                                    rs.getString(MIDDLE_NAME)),
+                            Position.valueOf(rs.getString(POSITION)),
+                            rs.getDate(HIREDATE).toLocalDate(),
+                            rs.getBigDecimal(SALARY),
                             getManager(rs)
                     );
                 }
