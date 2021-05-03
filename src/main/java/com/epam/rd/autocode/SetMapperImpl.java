df4d95c2022970ea06d074bf58058dae7eb171a4
DzmitryTysevich <dztysevich@gmail.com>
2021-05-03 20:07:39 +0300

    add manager method

--- src/main/java/com/epam/rd/autocode/SetMapperImpl.java
+++ src/main/java/com/epam/rd/autocode/SetMapperImpl.java
@@ -33,7 +33,42 @@ public class SetMapperImpl implements SetMapper<Set<Employee>> {
                 Position.valueOf(resultSet.getString("position")),
                 resultSet.getDate("hiredate").toLocalDate(),
                 resultSet.getBigDecimal("salary"),
-                null
+                getManager(resultSet)
         );
+    }
+
+    private Employee getManager(ResultSet resultSet) throws SQLException {
+        Employee manager = null;
+        try {
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:hsqldb:mem:myDb",
+                    "sa", "sa"
+            );
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from EMPLOYEE E left join EMPLOYEE N on E.MANAGER = N.ID");
+            while (rs.next()) {
+                if (resultSet.getInt("manager") == (rs.getInt("id"))) {
+                    manager = new Employee(
+                            new BigInteger(String.valueOf(rs.getInt("id"))),
+                            new FullName(
+                                    rs.getString("firstname"),
+                                    rs.getString("lastname"),
+                                    rs.getString("middleName")),
+                            Position.valueOf(rs.getString("position")),
+                            rs.getDate("hiredate").toLocalDate(),
+                            rs.getBigDecimal("salary"),
+                            getManager(rs)
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return manager;
     }
 }
